861e15af65e52ecc6b0f402606f7bbb1b9106bf4
wambui-nyambura <nyamburawambuui@gmail.com>
2023-03-09 08:59:32 +0300

    name

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+var result=character("Eldoret","Mombasa")
+    println(result)
+    call("Nairobi")
+    take()
+}
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length. (4 pts)
+fun character(x:String,y:String):String{
+    var expt=x==y
+
+
+
+fun numerals(num:Int) {
+    var x = num.maxOrNull()
+    var y = num.minOrNull()
+    var z = num.average()
+
+
+
+
+}
+fun call(x:String){
+    println(x.split(""))
+}
+
+fun take(y:Array<String>):String{
+    var z=(y.joinToString("Name","age","class"))
+    return (z)
+
+}
+
+
